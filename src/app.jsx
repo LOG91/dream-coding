af0842a10b1394b66e086b472fc43802bb4f9231
log91 <peter1636@naver.com>
2020-12-09 22:55:38 +0900

    save: 4-7

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Habit from './components/habit';
+import './app.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">hello js!</div>
+      <Habit />
+    </>
+  );
+}
+
+export default App;
